eaa6865b78f86686abadc2f484d592d51f7c2432
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-09-18 01:19:15 -0400

    refine Template routes, add sort order

--- controllers/api/template-routes.js
+++ controllers/api/template-routes.js
@@ -2,6 +2,7 @@
 // IMPORTS
 const router = require('express').Router();
 const {Template, Fillin, User} = require('../../models');
+const _ = require('lodash');
 
 
 
@@ -11,11 +12,11 @@ const {Template, Fillin, User} = require('../../models');
 router.get('/', async (req, res) => {
     try{
         var dbTemplatesData = await Template.findAll({
-            attributes: ['id', 'title', 'content', 'static_count', 'mutable_count', 'redaction_order', 'created_at'],
+            attributes: ['id', 'title', 'created_at'],
             include: [
                 {
                     model: User,
-                    attributes: ['id', 'username'],
+                    attributes: ['id', 'username']
                 },
                 {
                     model: Fillin,
@@ -23,19 +24,20 @@ router.get('/', async (req, res) => {
                     as: 'fillins',
                     include: {
                         model: User,
-                        attributes: ['id', 'username'],
-                    },
-                    // order: [['created_at', 'ASC']]
+                        attributes: ['id', 'username']
+                    }
                 }
+            ],
+            order: [
+                ['created_at', 'DESC'], // the default sort order for Templates (may be shuffled, see below)
+                [{model: Fillin, as: 'fillins'}, 'created_at', 'DESC'] // the default sort order for Fillins within each template
             ]
         });
 
-        dbTemplatesData = dbTemplatesData.map(template => {
-            let output = template.get({plain: true})
-            output.content = JSON.parse(output.content);
-            output.redaction_order = JSON.parse(output.redaction_order);
-            return output;
-        });
+        if (req.query.sortby === 'random')
+            dbTemplatesData = _.shuffle(dbTemplatesData);
+
+        dbTemplatesData = dbTemplatesData.map(template => template.get({plain: true}));
 
         res.json(dbTemplatesData);
     }catch (err){
@@ -45,7 +47,7 @@ router.get('/', async (req, res) => {
 });
 
 
-// Get one NORMAL
+// Get one
 router.get('/:id', async (req, res) => {
     try {
         var dbTemplateData = await Template.findByPk(req.params.id, {
@@ -53,7 +55,7 @@ router.get('/:id', async (req, res) => {
             include: [
                 {
                     model: User,
-                    attributes: ['id', 'username'],
+                    attributes: ['id', 'username']
                 },
                 {
                     model: Fillin,
@@ -61,13 +63,21 @@ router.get('/:id', async (req, res) => {
                     as: 'fillins',
                     include: {
                         model: User,
-                        attributes: ['id', 'username'],
+                        attributes: ['id', 'username']
                     },
-                    // order: [['createdAt', 'ASC']]
                 }
-            ]
+            ],
+            order: [[{model: Fillin, as: 'fillins'}, 'created_at', 'DESC']] // the default sort order for Fillins within this template
         });
 
+        if (!dbTemplateData){
+            res.status(404).json({message: 'No template found with this ID'});
+            return;
+        }
+
+        if (+req.query.redacted)
+            dbTemplateData = dbTemplateData.redactContent(+req.query.lvl)
+        
         dbTemplateData = dbTemplateData.get({plain: true});
         dbTemplateData.content = JSON.parse(dbTemplateData.content);
         dbTemplateData.redaction_order = JSON.parse(dbTemplateData.redaction_order);
@@ -75,44 +85,7 @@ router.get('/:id', async (req, res) => {
         res.json(dbTemplateData);
     }catch (err){
         console.log(err);
-        res.status(404).json(err);
-    }
-});
-
-
-// Get one REDACTED
-router.get('/redacted/:id', async (req, res) => {
-    try {
-        var dbTemplateData = await Template.findByPk(req.params.id, {
-            attributes: ['id', 'title', 'content', 'static_count', 'mutable_count', 'redaction_order', 'created_at'],
-            include: [
-                {
-                    model: User,
-                    attributes: ['id', 'username'],
-                },
-                {
-                    model: Fillin,
-                    attributes: ['id', 'created_at'],
-                    as: 'fillins',
-                    include: {
-                        model: User,
-                        attributes: ['id', 'username'],
-                    },
-                    // order: [['createdAt', 'ASC']]
-                }
-            ]
-        });
-
-        dbTemplateData = dbTemplateData
-            .redactContent(+req.query.lvl)
-            .get({plain: true});
-        dbTemplateData.content = JSON.parse(dbTemplateData.content);
-        dbTemplateData.redaction_order = JSON.parse(dbTemplateData.redaction_order);
-
-        res.json(dbTemplateData);
-    }catch (err){
-        console.log(err);
-        res.status(404).json(err);
+        res.status(500).json(err);
     }
 });
 
