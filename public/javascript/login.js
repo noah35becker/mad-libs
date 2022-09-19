4d6bd5c7f24c685c20048771084c9f6cc755b7b5
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-09-19 13:24:00 -0400

    move scripts from main.handlebars -> public/javascript; further styling

---
+++ public/javascript/login.js
@@ -0,0 +1,10 @@
+
+// Open login modal
+$('.nav-login').click(() =>
+    $('#login-modal').css('display', 'block')
+);
+
+// Close login modal
+$('.close-modal').click(() =>
+    $('#login-modal').css('display', 'none')
+);
