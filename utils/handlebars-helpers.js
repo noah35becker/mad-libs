68d4cf0aaae92e375a302b55b29081205d76cbec
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-09-19 03:13:45 -0400

    handlebars helper functions

--- utils/handlebars-helpers.js
+++ utils/handlebars-helpers.js
@@ -1,4 +1,15 @@
 
+const {DateTime} = require('luxon');
+
+
 module.exports = {
-    redaction_slider_width: redactionLvls => Math.min((redactionLvls + 1) * 26, 200)
+    format_date: date => {
+        return DateTime.fromISO(new Date(date).toISOString()).toFormat("MMM d, yyyy 'at' HH:mm:ss");
+    },
+
+    format_plural: (word, quantity) => quantity === 1 ? word : word+'s',
+
+    redaction_slider_width: redactionLvls => Math.min((redactionLvls + 1) * 26, 200), 
+
+    compare: (a, b) => a === b
 };
