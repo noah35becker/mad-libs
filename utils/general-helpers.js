9472d06a813dcd7105beec33050fe808bacca53f
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-09-20 01:40:28 -0400

    add Templates and Fill-ins to site menu

--- utils/general-helpers.js
+++ utils/general-helpers.js
@@ -1,28 +1,7 @@
 
-// When createdAt and updatedAt are identical for a given post, removes the updatedAt property
-// (this is important for how post-info.handlebars processes post data)
-function purgeUpdatedAtProperty(posts){
-    const isSinglePost = !Array.isArray(posts);
-
-    if (isSinglePost)
-        posts = [posts];
-
-    for (const post of posts){
-        if (post.createdAt.toString() === post.updatedAt.toString()){
-            delete post.updatedAt;
-        }
-    }
-
-    if (isSinglePost)
-        return posts[0];
-    return posts;
-}
-
-
 const sessionTimeout = 1000 * 60 * 15; // User's session expires after 15 minutes
 
 
 module.exports = {
-    purgeUpdatedAtProperty,
     sessionTimeout
 };
