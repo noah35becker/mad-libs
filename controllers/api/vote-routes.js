27f6fd264629e2d062e00f8a5bedc3c95796ad64
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-09-19 23:00:53 -0400

    POST and DELETE routes added for Vote

--- controllers/api/vote-routes.js
+++ controllers/api/vote-routes.js
@@ -5,7 +5,7 @@ const {Vote} = require('../../models');
 
 
 
-// REQUEST
+// REQUESTS
 
 // Check if a given User voted for a given Fillin
 router.get('/check', async (req, res) => {
@@ -17,7 +17,44 @@ router.get('/check', async (req, res) => {
             }
         });
 
-        res.json({status: (dbVoteData ? true : false)});
+        res.json({message: (dbVoteData ? true : false)});
+    }catch (err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+// Create new
+router.post('/', async (req, res) => {
+    try{
+        const dbVoteData = await Vote.create({
+            user_id: +req.session.user_id,
+            fillin_id: req.body.fillinId
+        });
+
+        res.json({
+            message: 'Vote successfully created',
+            comment: dbVoteData
+        });
+    }catch (err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+// Delete
+router.delete('/', async (req, res) => {
+    try{
+        await Vote.destroy({
+            where: {
+                user_id: +req.session.user_id,
+                fillin_id: req.body.fillinId
+            }
+        });
+
+        res.json({message: 'Vote successfully deleted'});
     }catch (err){
         console.log(err);
         res.status(500).json(err);
