49366ef36e0ec5258d1949965b0c6aaabec35dcc
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-09-20 22:53:48 -0400

    fix logout fetch route

--- public/javascript/user/logout.js
+++ public/javascript/user/logout.js
@@ -1,7 +1,7 @@
 
 // Logout
 $('.logout-nav').click(async () => {
-    const response = await fetch('api/user/logout',{
+    const response = await fetch(location.href.split(RegExp('(?<!\/)\/(?!\/)'))[0] + '/api/user/logout',{
         method: 'post'
     });
 
