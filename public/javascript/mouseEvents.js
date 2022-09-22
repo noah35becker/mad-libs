770ec04da0c1b0a289156fe4c4e873b154c409e1
claudia-j-yile <cshao@alumni.risd.edu>
2022-09-22 15:02:04 -0400

    basic mouse listener working

--- public/javascript/mouseEvents.js
+++ public/javascript/mouseEvents.js
@@ -1,38 +1,25 @@
-//vanilla js 
-const bgChange = document.querySelector(".body")
 
-var mutableColor: {
-    mut_red: 255,
-    mut_green: 0,
-    mut_blue: 0,
-}
+    var red = 0;
 
-var cardinalColor: {
-    car_red: 255,
-    car_green: 255,
-    car_blue: 255,
-}
 
-bgChange.addEventListener('mousemove', (e) => {
-    var updated_mut_red = mutableColor.mut_red - e.mouseEvent.screenX/10;
-    var change_x = updated_mut_red - mutableColor.mut_red;
-    if(updated_mut_red >=255 && mutableColor.mut_green >=0){
-        mutableColor.mut_red = updated_mut_red;
-        mutableColor.mut_green = mutableColor.mut_green + change_x;
-        mutableColor.mut_blue = mutableColor.mut_green;
-    }; else if(updated_mut_red <=0 && mutableColor.mut_green >=255){
-        mutableColor.mut_red = mutableColor + change_x;
-        mutableColor.mut_green = mutableColor.mut_green - change_x;
-        mutableColor.mut_blue = mutableColor.mut_green;
-    } 
-}); 
+/*parse whether colors are in optimal state for change 
+    Number.Prototype.between = function(100, 222) {
+    let min = Math.min.apply(Math, [100, 222]),
+      max = Math.max.apply(Math, [100, 222]);
+      return this >= min && this <= max; 
+  };
 
-bgChange.addEventListener('mousemove', , (e) => {
-    var updated_car_red = cardinalColor.car_red - e.mouseEvent.screenY/10;
-    var change_y = updated_car_red - cardinalColor.car_red;
-    if(updated_car_red >=255 && cardinalColor.car_green >=0){
-    }; else if(updated_mut_red <=0 && mutableColor.mut_green >=255){
+    red.between();
+*/
+
+$('.body').on('mousemove', (e) => {
+    if(red <=250){
+        red++;
+        console.log("RGBs now variable");
+    }; else if (red >=251){
+        red--;
     }
-}
-    );
+    }); 
 
+// e.mousemove.screenX;
+*/
